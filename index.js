23f329ac1d656afc3c9f66e478dbdf50287271fa
ebharathi <elayabharathi10102002@gmail.com>
2023-11-26 01:18:04 +0530

    syntax fix:

--- index.js
+++ index.js
@@ -21,71 +21,66 @@ let users=[];
     //on connecting to the server
        //create a user
           io.on('connection',(socket)=>{
-              console.log("New client connected[+] ",socket.id)
-            // emit user details every 60 sec
-            // Set up a timer to emit a socket event every 5 seconds
-            const emitInterval = setInterval(() => {
-                // Emit the event to the connected
-                // console.log("Emitting data[+]") 
-                io.emit('userConnected', users);
-            }, 10000);
-              //new user connection
-              socket.on('setNewUser',({id,username,bg})=>{
-                const isUserPresent=io.sockets.sockets.get(id);
-                if(isUserPresent)
-                {
-                    isUserPresent.username=username;
-                    isUserPresent.bg=bg;
-                    let isChanged=false;
-                    let empArray=[];
-                    users.map((single)=>{
-                        if(single.id==id)
-                        {
-                            //update the details of the user in server if present
-                            let newsingle={id:id,username:username,bg:bg}
-                            empArray.push(newsingle)
-                            isChanged=true;
-                        }
-                        else
-                          empArray.push(single)
-                    })
-                    if(isChanged==false)
-                      users.push({id:id,username:username,bg:bg})
-                    else 
-                      users=empArray
-                    //return all the users currently in the server
-                    console.log("USER ARRAY UPDATED-->",users)
-                    io.emit('userConnected', users);
-                }
-            })
-            //remove the user
-            socket.on('disconnect',()=>{
-                users=users.filter((item)=>item.id!=socket.id)
-                console.log("uer disconnected and array updated")
-                console.log("user--.",users)
-                clearInterval(emitInterval)
-                io.emit('userConnected', users);
-              })
-              //create room
-              socket.on("join_room",(id)=>{
-                console.log("Joining into a room id->",id)  
-                socket.join(id)
-              })
-              //send message
-              socket.on('send_message',(data)=>{
-                     console.log("[+] sending msg-->",data)
-                     socket.to(data.id).emit('receive_message',data)
-              })
-              //liked message
-              socket.on("liked_message",(data)=>{
-                     console.log("liked msg-->",data)
-                     socket.to(data.id).emit("receive_message",data)
-              })
-              //delete message
-              socket.on("delete_message",(data)=>{
-                  console.log("Deleted msg-->",data)
-                  socket.to(data.id).emit("receive_deleted_message",data);
-              })
+              // emit list of users every 60 sec
+                    const emitInterval = setInterval(() => {
+                        // Emit the event to the connected
+                        // console.log("Emitting data[+]") 
+                        io.emit('userConnected', users);
+                    }, 10000);
+                    //new user connection
+                        console.log("New client connected[+] ",socket.id)
+                        socket.on('setNewUser',({id,username,bg})=>{
+                            const isUserPresent=io.sockets.sockets.get(id);
+                            if(isUserPresent)
+                            {
+                                let isChanged=false;
+                                let empArray=[];
+                                users.map((single)=>{
+                                    if(single.id==id)
+                                    {
+                                        //update the details of the user in server if present
+                                        let newsingle={id:id,username:username,bg:bg}
+                                        empArray.push(newsingle)
+                                        isChanged=true;
+                                    }
+                                    else
+                                    empArray.push(single)
+                                })
+                                if(isChanged==false)
+                                users.push({id:id,username:username,bg:bg})
+                                else 
+                                users=empArray
+                                //return all the users currently in the server
+                                io.emit('userConnected', users);
+                            }
+                        })
+                    //remove the user
+                        socket.on('disconnect',()=>{
+                            users=users.filter((item)=>item.id!=socket.id)
+                            clearInterval(emitInterval)
+                            io.emit('userConnected', users);
+                        })
+                    //create room
+                        socket.on("join_room",(id)=>{
+                            console.log("Joining into a room id->",id)  
+                            socket.join(id)
+                        })
+                    //send message
+                        socket.on('send_message',(data)=>{
+                                console.log("[+] sending msg-->",data)
+                                socket.to(data.id).emit('receive_message',data)
+                        })
+                    //liked message
+                        socket.on("liked_message",(data)=>{
+                                console.log("liked msg-->",data)
+                                socket.to(data.id).emit("receive_message",data)
+                        })
+                    //delete message
+                        socket.on("delete_message",(data)=>{
+                            console.log("Deleted msg-->",data)
+                            socket.to(data.id).emit("receive_deleted_message",data);
+                        })
           })
+          
 const PORT=9000;
 server.listen(PORT,()=>console.log("Socket Server running on port: ",PORT))
